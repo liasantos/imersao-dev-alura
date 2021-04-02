99d0e71d12f75e00c5cbb511efc834617d367988
Lia Santos <liasilva77@acad.charqueadas.ifsul.edu.br>
2021-04-02 17:21:19 -0300

    Enviando arquivos da aula 4 :movie_camera:

---
+++ aluraflix-arrays/script.js
@@ -0,0 +1,51 @@
+var listaFilmes = [
+  "https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
+
+
+/*aprendizado*/
+
+//var filme1 = "Star Wars";
+//var filme2 = "Toy Story";
+//var filme3 = "Interestellar";
+//console.log(filme1)
+//console.log(filme2)
+//console.log(filme3)
+
+/*//laço de repetição while
+//var tentativas = 3;
+while (tentativas > 0){
+  
+  tentativas = tentativas - 1;
+}*/
+
+/*//laço de repetição for
+//filmes.lenght faz com que atualize automático o laço, independente de quantos push forem feitos na array
+var filmes = ["Star Wars", "Toy Story", "Interestellar", "Esqueceram de Mim", "Marley e Eu"];
+for (var i = 0; i < filmes.length; i++){
+  console.log(filmes[i]);
+}*/
+
+//console.log(filmes[0]);
+
+/*//pega o elemento da array conforme posição:
+var filmes = ["Star Wars", "Toy Story", "Interestellar"];
+console.log(filmes[0]);
+console.log(filmes[1]);
+console.log(filmes[2]);
+//conta a largura da array (quantos elementos):
+console.log(filmes.length);*/
+
+/*//cria uma array vazia
+var filmes2 = [];
+//coloca elementos na array
+filmes2.push("Star Wars");
+filmes2.push("Toy Story");
+filmes2.push("Interestellar");
+console.log(filmes2);*/
